335fa1720f6b6a6f4c514cc0e366001f5837f9f9
Davi Souza <davi.231souza@gmail.com>
2025-02-07 16:21:41 -0300

    fix: bug

--- lib/data/repositories/tasks_repository_dev/tasks_repository_dev.dart
+++ lib/data/repositories/tasks_repository_dev/tasks_repository_dev.dart
@@ -2,23 +2,28 @@ import 'package:app_tasks_challenge/data/repositories/tasks_repository.dart';
 import 'package:app_tasks_challenge/domain/models/task.dart';
 import 'package:result_dart/result_dart.dart';
 
-class TasksRepositoryDev implements TasksRepository { 
+class TasksRepositoryDev implements TasksRepository {
+  final List<Task> _list = [];
+
   @override
-  // Future<Result<List<Task>>> get() {
-    
+  Future<Result<List<Task>>> get() {
+    throw UnimplementedError();
+  }
 
-  // }
+  @override
+  Future<Result<Task>> add(Task task) {
+    final lastTaskIndex = _list.length;
 
-  // @override 
-  // Future<Result<Task>> add(List<Task> tasks) {
-  //   final lastTaskIndex = tasks.length;
+    final Task createdTask = Task(
+        id: lastTaskIndex + 1,
+        title: task.title,
+        description: task.description);
 
-  //   final Task createdTask = Task(title: task, description: description)
-  // }
-  
+    throw UnimplementedError();
+  }
 
-  // @override 
-  // Future<Result<String>> delete(Task task) {
-
-  // }
+  @override
+  Future<Result<String>> delete(Task task) {
+    throw UnimplementedError();
+  }
 }
